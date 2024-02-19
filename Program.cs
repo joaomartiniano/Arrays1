fde2ccea6f2b1fda61cedf20c52820f4da4cfce9
João Martiniano <44736477+joaomartiniano@users.noreply.github.com>
2024-02-19 17:31:31 +0000

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = { 30, 5, -33, 0, 10, 7, 79, 18, -9, 45, 1300, 15, 0, 814 };
+
+            string[] localidades =  { "LISBOA", "PORTO", "VIANA DO CASTELO", "BRAGA", "ESTORIL",
+                                      "SETÚBAL", "GUIMARÃES", "COIMBRA", "CASCAIS", "MATOSINHOS",
+                                      "FARO", "LEIRIA", "AVEIRO", "VISEU", "TORRES NOVAS",
+                                      "TORRES VEDRAS", "OEIRAS", "FUNCHAL", "ÉVORA", "PORTIMÃO", "SINTRA",
+                                      "SANTARÉM", "VILA REAL", "PONTA DELGADA", "BEJA", "ALCABIDECHE",
+                                      "VILA NOVA DE GAIA", "SENHORA DA HORA", "OVAR", "PAREDES", "ALMADA",
+                                      "SANTA MARIA DA FEIRA", "SÃO COSME", "GUARDA", "COVILHÃ" };
+
+            #region "Ordenação"
+            // Ordenar o array numeros
+
+            // *** Inserir código C#
+
+            // Mostrar o array numeros, ordenado
+            foreach (int n in numeros)
+            {
+                // *** Inserir código C#
+            }
+
+            // Ordenar o array localidades
+
+            // *** Inserir código C#
+
+            // Mostrar o array localidades, ordenado
+
+            // *** Inserir código C#
+            #endregion
+
+            #region "Efetuar pesquisas"
+
+            #endregion
+
+            #region "Inverter"
+
+            // Inverter o array numeros
+
+            // *** Inserir código C#
+
+            // Mostrar o array numeros, invertido
+            foreach (int n in numeros)
+            {
+                // *** Inserir código C#
+            }
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
